0de1a18e42f675ad50cd8ba95d0057e876c5bddf
imalzy <imalzaya19@gmail.com>
2024-03-02 04:47:04 +0700

    add error exeption

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
-
 class Handler extends ExceptionHandler
 {
     /**
@@ -26,5 +27,32 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'This Method is not allowed for the requested route',
+                'status' => '405',
+            ], 405);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json( [
+                'success' => 0,
+                'message' => 'This Route is not found',
+                'status' => '404',
+            ], 404 );
+        }
+
+        if ($exception instanceof \BadMethodCallException) {
+            return response()->json( [
+                'success' => 0,
+                'message' => 'Bad Method Called',
+                'status' => '404',
+            ], 404 );
+        }
     }
 }
